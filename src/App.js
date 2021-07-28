3d774fa7aa2c86fb6ae2a74db7f0a272d0f024d9
Aaron Koivunen <aaron@karbook.com>
2021-07-28 09:58:41 -0500

    Remove button2 and add a header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from"react";
-import SimpleButton from './button';
-import SimpleButton2 from "./Button2";
+import React from "react";
+import SimpleButton from "./button";
+// import SimpleButton2 from "./Button2";
 
 function App() {
   return (
     <div className="App">
-    <SimpleButton/>
-    <SimpleButton2/>
+      <h1>Welcome</h1>
+      <SimpleButton />
+      {/* <SimpleButton2/> */}
     </div>
   );
 }
